46c8fdef7103349fe730b98e8f4b642a9e034f85
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-04 18:13:43 +0530

    Sign Up screen

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,78 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, Image, Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButtom from "../../components/CustomButton";
 
 const SignUp = () => {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+    const [submitting, isSubmitting] = useState(false);
+
+    const submit = () => {};
+
     return (
-        <View>
-            <Text>SignUp</Text>
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView>
+                <View
+                    className="w-full justify-center min-h-[90vh]
+                px-4 my-6"
+                >
+                    <Image
+                        source={images.logo2}
+                        resizeMode="containe"
+                        className="w-[120px] h-[37px]"
+                    />
+                    <Text
+                        className="text-white text-2xl 
+                    text-semibold font-psemibold
+                    mt-10"
+                    >
+                        Sign up to Yoru
+                    </Text>
+                    <FormField
+                        title="Username"
+                        value={form.username}
+                        handleChangeText={(e) => setForm({ ...form, username: e })}
+                        otherStyles="mt-7"
+                    />
+                    <FormField
+                        title="Email"
+                        value={form.email}
+                        handleChangeText={(e) => setForm({ ...form, email: e })}
+                        otherStyles="mt-7"
+                        keyboardType="email-address"
+                    />
+                    <FormField
+                        title="Password"
+                        value={form.password}
+                        handleChangeText={(e) => setForm({ ...form, password: e })}
+                        otherStyles="mt-7"
+                    />
+                    <CustomButtom
+                        title="Sign Up"
+                        handlePress={submit}
+                        containerStyles="mt-7"
+                        isLoading={submitting}
+                    />
+                    <View className="justify-center pt-5 flex-row gap-2">
+                        <Text className="text-lg text-gray-100 font-pregular">
+                            Already have an account?
+                        </Text>
+                        <Link href="/sign-in" className="text-lg font-psemibold text-secondary">
+                            Sign In
+                        </Link>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 export default SignUp;
-
-const styles = StyleSheet.create({});
