5507fe38ba558a1f370b03d0ca1a6576e8d416cb
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-28 10:16:06 +0530

    Typescript setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="">
+            <Text className="text-3xl">Test</Text>
+        </View>
+    );
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
     theme: {
-        extend: {
-            colors: {
-                primary: "#161622",
-                secondary: {
-                    DEFAULT: "#fa5252",
-                    100: "#fa5252",
-                    200: "#ff0138",
-                },
-                black: {
-                    DEFAULT: "#000",
-                    100: "#1E1E2D",
-                    200: "#232533",
-                },
-                gray: {
-                    100: "#CDCDE0",
-                },
-            },
-            fontFamily: {
-                pthin: ["Poppins-Thin", "sans-serif"],
-                pextralight: ["Poppins-ExtraLight", "sans-serif"],
-                plight: ["Poppins-Light", "sans-serif"],
-                pregular: ["Poppins-Regular", "sans-serif"],
-                pmedium: ["Poppins-Medium", "sans-serif"],
-                psemibold: ["Poppins-SemiBold", "sans-serif"],
-                pbold: ["Poppins-Bold", "sans-serif"],
-                pextrabold: ["Poppins-ExtraBold", "sans-serif"],
-                pblack: ["Poppins-Black", "sans-serif"],
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
